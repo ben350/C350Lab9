a91e0fde3efcd08aa7326e06b5c396a2f391e5eb
Benji Ramirez <benjiramirez@Benjis-MacBook-Pro.local>
2015-04-06 23:20:20 -0700

    final message, working on making software using maven.

---
+++ Voxam/src/test/java/nostalgic/textadv/voxam/ParserSteps.java
@@ -0,0 +1,39 @@
+package nostalgic.textadv.voxam;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+import cucumber.api.PendingException;
+
+public class ParserSteps {
+  @Given("^the game parser$")
+  public void the_game_parser() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @When("^the command \"(.*?)\" is parsed$")
+  public void the_command_is_parsed(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Then("^the verb is \"(.*?)\"$")
+  public void the_verb_is(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Then("^there is no direct object$")
+  public void there_is_no_direct_object() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Then("^the parser response is \"(.*?)\"$")
+  public void the_parser_response_is(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+}
